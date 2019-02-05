31c6eaa2a51006475a996b0f958544e7527a236d
ChenouZhang <42550849+ChenouZhang@users.noreply.github.com>
2019-02-05 14:59:50 -0700

    ABMD Traj Script V0.0.1

--- ABMD_test.py
+++ ABMD_test.py
@@ -1,6 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import MDAnalysis as mda
+from MDAnalysis.analysis import align
+from MDAnalysis.analysis.rms import rmsd
+from MDAnalysis.analysis.rms import RMSD
 
-#Python scripts to read,plot 2D_umbrella_sampling result.
-#Add WHAM-2d read and plot.
+#Python scripts to ABMD traj.
+#Help to plot ddRMSD
+
+################ Load Data ###################
+def Load_Traj(PDB_Ini,XTC = None,PDB_End):
+    '''
+    Parameters: 
+    	PDB_Ini: Input Ini_structure file name
+    	PDB_End: Input End_structure file name
+        XTC: Traj file comes with PDB_Ini   
+	Returns:
+        ABMD: Full traj
+    	Ini_ref: Initial frame
+        End_ref: Target frame
+    '''
+    ABMD = mda.Universe(PDB_Ini,XTC)
+    End_ref = mda.Universe(PDB_End)
+    for ts in ABMD.trajectory[0]:
+        Ini_ref = ABMD.atoms
+
+    return ABMD,Ini_ref,End_ref
+
+################ Initialize parameters ################
+def Load_Traj(Ini_ref,End_ref):
+	'''
+	Parameters: 
+		Ini_ref: Initial frame
+        End_ref: Target frame
+	Returns:
+        rmsd_ab: rmsd between start and end frame
+		Ini_position: Coord of initial frame
+        End_position: Coord of target frame
+	'''
+    bni_temp = Ini_ref.select_atoms('name CA')
+    End_temp = End_ref.select_atoms('name CA')
+    Ini_position = Ini_temp.positions
+    End_position = End_temp.positions
+    align.alignto(Ini_temp,End_temp)
+    rmsd_ab = rmsd(Ini_position,End_position)
+    return rmsd_ab,Ini_position,End_position
+
+ddRMSD = []                        # ddRMSD
+
+################ Functions ####################
+RMSD_ini = RMSD(ABMD,Ini_ref,select='name CA')
+RMSD_ini.run()
+RMSD_end = RMSD(ABMD,End_ref,select='name CA')
+RMSD_end.run()
+rmsd_ini = RMSD_ini.rmsd.T
+rmsd_end = RMSD_end.rmsd.T
+
+time = rmsd_ini[1]
+ddRMSD = (rmsd_ini[2]-rmsd_end[2])/rmsd_ab
+
+
