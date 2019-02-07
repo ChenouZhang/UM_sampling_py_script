9864d32f0f9cf64176ef5dcfdc65ec45a22ee772
ChenouZhang <42550849+ChenouZhang@users.noreply.github.com>
2019-02-07 15:34:24 -0700

    Useful head import file.

---
+++ Head.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import MDAnalysis as mda
+from MDAnalysis.analysis import align
+from MDAnalysis.analysis.rms import rmsd
+from MDAnalysis.analysis.rms import RMSD
+from os import path
